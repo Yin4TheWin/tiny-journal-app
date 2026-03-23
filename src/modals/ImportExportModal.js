fa29a5a50c973f007c66648d7b996fbdfeb0c395
Franklin Yin <yin4thewin@gmail.com>
2026-03-23 03:06:09 -0700

    Change export/import icons

--- src/modals/ImportExportModal.js
+++ src/modals/ImportExportModal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Container, Row, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileImport, faFileExport } from '@fortawesome/free-solid-svg-icons';
+import { faUpload, faDownload } from '@fortawesome/free-solid-svg-icons';
 import { validateJournalData } from '../store/journalValidator';
 
 function ImportExportModal({ show, onHide, savedNotes, dispatch }) {
@@ -93,7 +93,7 @@ function ImportExportModal({ show, onHide, savedNotes, dispatch }) {
                 <p className="mb-0">Export your journal entries so they can be accessed from another device. Exporting <strong>PRESERVES</strong> encryption.</p>
               </Col>
               <Col xs={4}>
-                <Button variant="outline-primary" className="w-100" onClick={exportJournal} disabled={isImporting}><FontAwesomeIcon icon={faFileExport} /> Export</Button>
+                <Button variant="outline-primary" className="w-100" onClick={exportJournal} disabled={isImporting}><FontAwesomeIcon icon={faUpload} /> Export</Button>
               </Col>
             </Row>
 
@@ -102,7 +102,7 @@ function ImportExportModal({ show, onHide, savedNotes, dispatch }) {
                 <p className="mb-0">Import your journal entries from another device and <strong>REPLACE</strong> the current journal entries.</p>
               </Col>
               <Col xs={4}>
-                <Button variant="outline-danger" className="w-100" onClick={importReplace} disabled={isImporting}><FontAwesomeIcon icon={faFileImport} /> Import (Replace)</Button>
+                <Button variant="outline-danger" className="w-100" onClick={importReplace} disabled={isImporting}><FontAwesomeIcon icon={faDownload} /> Import (Replace)</Button>
               </Col>
             </Row>
 
@@ -111,7 +111,7 @@ function ImportExportModal({ show, onHide, savedNotes, dispatch }) {
                 <p className="mb-0">Import your journal entries from another device and <strong>MERGE</strong> them with your current entries, so both will be present.</p>
               </Col>
               <Col xs={4}>
-                <Button variant="outline-success" className="w-100" onClick={importMerge} disabled={isImporting}><FontAwesomeIcon icon={faFileImport} /> Import (Merge)</Button>
+                <Button variant="outline-success" className="w-100" onClick={importMerge} disabled={isImporting}><FontAwesomeIcon icon={faDownload} /> Import (Merge)</Button>
               </Col>
             </Row>
           </Container>
